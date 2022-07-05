99550934796fc25d73deb35c6b3feae79e6e6974
AntonioHincapie <marcohincapiem@gmail.com>
2022-07-05 15:20:39 -0500

    Add logo and Header

---
+++ src/navbar.js
@@ -0,0 +1,10 @@
+import logo from './planet.png';
+
+export default function NavBar() {
+  return (
+    <div className="Header">
+      <img src={logo} alt="logo" />
+      <h1>Space Travelers&apos; Hub</h1>
+    </div>
+  );
+}
