09f876c3e04dbbebae611a9d182f97285ea4c767
franclobo <fjbl2788@gmail.com>
2022-07-05 16:57:15 -0500

    Create component Dragons.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React from 'react';
+import {
+  Route,
+  Routes,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
 import NavBar from './components/navbar';
+import MyProfile from './components/MyProfile';
+import Dragons from './components/Dragons';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <Provider>
+      <NavBar className="header" />
+      <Routes>
+        <Route
+          path="/dragons"
+          element={<Dragons />}
+        />
+        <Route
+          path="/myprofile"
+          element={<MyProfile />}
+        />
+      </Routes>
+    </Provider>
   );
 }
 
