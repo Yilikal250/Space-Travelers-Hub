1d6e16fd81b69e527964c14fb57c782a5a7df796
franclobo <fjbl2788@gmail.com>
2022-07-06 10:12:06 -0500

    Add Dragons link on NavBar.

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,7 +1,12 @@
+import NavBar from "./navbar";
+
 export default function Dragons() {
   return (
-    <div className="dragons-container">
-      <h1>Dragons</h1>
+    <div>
+      <NavBar />
+      <div className="dragons-container">
+        <h1>Dragons</h1>
+      </div>
     </div>
   );
 }
