c37621f79b3691e7486dff9b3681819e7ce15730
franclobo <fjbl2788@gmail.com>
2022-07-06 18:38:55 -0500

    Undone changes

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,23 +1,10 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
 import NavBar from './navbar';
 
-function MyProfile() {
-  const dragon = useSelector((state) => state.dragons);
-  const dragonList = dragon.filter((item) => item.reserved === true);
+export default function MyProfile() {
   return (
-    <>
+    <div>
       <NavBar />
-      <h1>My Dragons</h1>
-      <div>
-        {dragonList.map((item) => (
-          <div className="dragons-myprofile-item" key={item.id}>
-            <p>{item.name}</p>
-          </div>
-        ))}
-      </div>
-    </>
+      <h1>My Profile</h1>
+    </div>
   );
 }
-
-export default MyProfile;
