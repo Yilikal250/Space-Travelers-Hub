1593a4796bdb2975bc87920b0fe7e4bce74b9bfa
AntonioHincapie <marcohincapiem@gmail.com>
2022-07-06 15:00:46 -0500

    Add reserve rocket action

--- src/components/rocket.js
+++ src/components/rocket.js
@@ -1,15 +1,56 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { ReserveRocket } from '../Redux/Rockets/Rockets';
 
 function Rocket(props) {
   const {
-    id, name, description, img,
+    id, name, description, img, reserved,
   } = props;
+
+  const dispatch = useDispatch();
+
+  const ReserveRocketHandler = () => {
+    dispatch(ReserveRocket(id));
+  };
 
   return (
     <div className="rocket-container" id={id}>
       <img src={img} alt="Rocket" />
       <h3>{name}</h3>
-      <p>{description}</p>
+      <div className="description">
+        {reserved && (
+          <div>
+            <p>
+              <span className="reserved">Reserved</span>
+              {description}
+            </p>
+          </div>
+        )}
+        {!reserved && (
+          <div>
+            <p>{description}</p>
+          </div>
+        )}
+      </div>
+      {reserved && (
+        <button
+          type="button"
+          className="reservedBtn"
+        >
+          It&apos;s Reserved
+        </button>
+      )}
+      {!reserved && (
+        <button
+          type="button"
+          className="reserveBtn"
+          onClick={() => {
+            ReserveRocketHandler();
+          }}
+        >
+          Reserve Rocket
+        </button>
+      )}
     </div>
   );
 }
@@ -19,6 +60,9 @@ Rocket.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
+  reserved: PropTypes.bool,
 };
+
+Rocket.defaultProps = { reserved: false };
 
 export default Rocket;
